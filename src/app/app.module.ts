75b65c1af4989e23ae291ca03cf8670a3509c62a
Iulia Popescu <iulia.popescu0720@gmail.com>
2021-02-16 17:11:30 +0200

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {DishdetailComponent} from './dishdetail/dishdetail.component';
 
+import {DishService} from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +34,9 @@ import {DishdetailComponent} from './dishdetail/dishdetail.component';
     MatButtonModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
